a70926a94ba6b7f5dff370dd47f37ad7cb95d956
rodrigoluizzz <rlfrancoassis@gmail.com>
2021-09-24 21:47:21 -0300

    Adicionado banco H2

--- src/main/java/com/rodrigo/cursomc/domain/Categoria.java
+++ src/main/java/com/rodrigo/cursomc/domain/Categoria.java
@@ -2,13 +2,22 @@ package com.rodrigo.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String nome;
 	
 	public Categoria(Integer id, String nome) {
