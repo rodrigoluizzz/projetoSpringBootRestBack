72f02c6ce50d9302feed24de99e758b56832e2b7
rodrigoluizzz <rlfrancoassis@gmail.com>
2022-01-05 18:40:59 -0300

    PUT e DELETE de categoria adicionado

--- src/main/java/com/rodrigo/cursomc/services/CategoriaService.java
+++ src/main/java/com/rodrigo/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.rodrigo.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.rodrigo.cursomc.domain.Categoria;
 import com.rodrigo.cursomc.repositories.CategoriaRepository;
+import com.rodrigo.cursomc.services.exceptions.DataIntegrityException;
 import com.rodrigo.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -15,7 +17,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: "+id+", Tipo: "+Categoria.class.getName()));
@@ -24,6 +26,21 @@ public class CategoriaService {
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 	
 }
