876718576c8758db10d52b9e825e05a83b3f4bc7
rodrigoluizzz <rlfrancoassis@gmail.com>
2022-01-25 10:36:21 -0300

    toString de pedidos

--- src/main/java/com/rodrigo/cursomc/services/PedidoService.java
+++ src/main/java/com/rodrigo/cursomc/services/PedidoService.java
@@ -14,7 +14,6 @@ import com.rodrigo.cursomc.domain.Pedido;
 import com.rodrigo.cursomc.repositories.ItemPedidoRepository;
 import com.rodrigo.cursomc.repositories.PagamentoRepository;
 import com.rodrigo.cursomc.repositories.PedidoRepository;
-import com.rodrigo.cursomc.repositories.ProdutoRepository;
 import com.rodrigo.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,13 +29,13 @@ public class PedidoService {
 	private PagamentoRepository pagamentoRepository;
 	
 	@Autowired
-	private ProdutoRepository produtoRepository;
-
-	@Autowired
 	private ProdutoService produtoService;
 	
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private ClienteService clienteService;
 
 	public Pedido buscar(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
@@ -48,6 +47,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if(obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -58,11 +58,13 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		for(ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoService.buscar(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoService.buscar(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
 
